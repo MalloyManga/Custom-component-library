514826ea4822e61616ca31b93584003a5ecdfebf
Malloy <li3210361050@gmail.com>
2025-07-18 16:18:53 +0800

    fix: change some files name

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Custome-component-library/', 
+  base: '/Custom-component-library/', 
   plugins: [
     vue(),
     vueDevTools(),
