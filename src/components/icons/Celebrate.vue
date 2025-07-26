44597226d2bcbeccd4ffcb74d2c82d91532a8046
Malloy <li3210361050@gmail.com>
2025-07-27 03:33:30 +0800

    feat: add some icons

---
+++ src/components/icons/Celebrate.vue
@@ -0,0 +1,11 @@
+<!-- Celebrate.vue -->
+<template>
+    <svg width="100" height="100" stroke="currentColor" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="10,10 20,20"></polygon>
+        <polygon points="10,50 20,50"></polygon>
+        <polygon points="20,80 30,70"></polygon>
+        <polygon points="90,10 80,20"></polygon>
+        <polygon points="90,50 80,50"></polygon>
+        <polygon points="80,80 70,70"></polygon>
+    </svg>
+</template>
