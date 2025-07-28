b6b1cb3f93d7c476c685eb6490be653da5b543f7
Malloy <li3210361050@gmail.com>
2025-07-29 00:00:34 +0800

    feat: add a new button

---
+++ src/components/Button19.vue
@@ -0,0 +1,26 @@
+<!-- Button19.vue -->
+<script setup lang="ts">
+import BilibiliUpload from './icons/BilibiliUpload.vue'
+
+interface Prop {
+    btn19Content: string
+}
+
+const { btn19Content = '投稿' } = defineProps<Prop>()
+
+</script>
+
+<template>
+    <div>
+        <button type="button"
+            class="flex justify-center items-center gap-2 bg-[#FB7299] px-6 py-2 rounded-xl cursor-pointer transition-all duration-300 hover:brightness-115">
+            <BilibiliUpload class="text-white" />
+            <span class="text-white">
+                {{ btn19Content }}
+            </span>
+        </button>
+    </div>
+
+
+
+</template>
--- src/views/ButtonView.vue
+++ src/views/ButtonView.vue
@@ -20,6 +20,7 @@ import Button17 from "@/components/Button17.vue"
 import Switch3 from "@/components/Switch3.vue"
 import Switch4 from "@/components/Switch4.vue"
 import Button18 from "@/components/Button18.vue"
+import Button19 from "@/components/Button19.vue"
 
 
 interface BtnComps {
@@ -47,6 +48,7 @@ const buttonComps: BtnComps[] = [
     { comp: Switch3 },
     { comp: Switch4 },
     { comp: Button18, btnProps: { posi: 'left' }, cardProps: { isDark: true } },
+    { comp: Button19, btnProps: { btn19Content: '投稿' } }
 ]
 
 </script>
